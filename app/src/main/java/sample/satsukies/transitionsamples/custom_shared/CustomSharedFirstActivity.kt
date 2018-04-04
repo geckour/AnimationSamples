f90dda3bbc9ad294fecfc4b3c82a176c0c182c93
geckour <geckour@gmail.com>
2018-04-04 16:21:02 +0900

    Fix bugs

--- app/src/main/java/sample/satsukies/transitionsamples/custom_shared/CustomSharedFirstActivity.kt
+++ app/src/main/java/sample/satsukies/transitionsamples/custom_shared/CustomSharedFirstActivity.kt
@@ -2,24 +2,31 @@ package sample.satsukies.transitionsamples.custom_shared
 
 import android.content.Intent
 import android.databinding.DataBindingUtil
+import android.os.Bundle
 import android.support.v4.app.ActivityOptionsCompat
 import android.support.v4.util.Pair
 import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.view.View
 import sample.satsukies.transitionsamples.R
 import sample.satsukies.transitionsamples.databinding.ActivityCustomSharedFirstBinding
 
 class CustomSharedFirstActivity : AppCompatActivity() {
 
     private val adapter: MyListAdapter by lazy {
-        MyListAdapter(this, mutableList) { v ->
-            val optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(this,
-                    Pair(v, v.transitionName))
+        MyListAdapter(this, mutableList, object : MyListAdapter.Listener {
+            override fun onClickItem(v: View) {
+                val optionsCompat =
+                        ActivityOptionsCompat.makeSceneTransitionAnimation(
+                                this@CustomSharedFirstActivity,
+                                Pair(v, v.transitionName))
 
-            val intent = Intent(this, CustomSharedSecondActivity::class.java)
-            startActivity(intent, optionsCompat.toBundle())
-        }
+                val intent = Intent(this@CustomSharedFirstActivity, CustomSharedSecondActivity::class.java)
+                startActivity(intent, optionsCompat.toBundle())
+            }
+
+        })
     }
+
     private val mutableList: MutableList<String> = mutableListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
 
     lateinit var binding: ActivityCustomSharedFirstBinding
--- app/src/main/java/sample/satsukies/transitionsamples/custom_shared/MyListAdapter.kt
+++ app/src/main/java/sample/satsukies/transitionsamples/custom_shared/MyListAdapter.kt
@@ -1,7 +1,7 @@
 package sample.satsukies.transitionsamples.custom_shared
 
+import android.content.Context
 import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
@@ -10,18 +10,14 @@ import android.widget.ArrayAdapter
 import sample.satsukies.transitionsamples.R
 import sample.satsukies.transitionsamples.databinding.ListItemBinding
 
-class MyListAdapter(private val context: AppCompatActivity, private val items: List<String>,
-                    private val listener: Listener) : ArrayAdapter<String>(context, LAYOUT_ID, items) {
+class MyListAdapter(
+        context: Context,
+        private val items: List<String>,
+        private val listener: Listener)
+    : ArrayAdapter<String>(context, LAYOUT_ID, items) {
 
     companion object {
         private const val LAYOUT_ID = R.layout.list_item
-    }
-
-    private lateinit var adapter: RecyclerAdapter
-    private val manager: LinearLayoutManager by lazy {
-        LinearLayoutManager(context).apply {
-            orientation = LinearLayoutManager.HORIZONTAL
-        }
     }
 
     interface Listener {
@@ -30,17 +26,32 @@ class MyListAdapter(private val context: AppCompatActivity, private val items: L
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
-        val listItemBinding: ListItemBinding =
+        val listItemBinding =
                 convertView?.let {
                     DataBindingUtil.getBinding<ListItemBinding>(convertView)
-                } ?: DataBindingUtil.inflate(LayoutInflater.from(context), LAYOUT_ID, parent, false)
+                }
+                        ?: DataBindingUtil.inflate<ListItemBinding>(
+                                LayoutInflater.from(context),
+                                LAYOUT_ID,
+                                parent,
+                                false
+                        ).apply {
+                            listTitle.text = "${items[position]}番目のレイアウトだよ"
 
-        listItemBinding.listTitle.text = "${items[position]}番目のレイアウトだよ"
+                            val adapter =
+                                    RecyclerAdapter(context, position, items, object : RecyclerAdapter.MyViewHolder.Listener {
+                                        override fun onClickItem(v: View) {
+                                            listener.onClickItem(v)
+                                        }
+                                    })
 
-        adapter = RecyclerAdapter(context, position, items) { v -> listener.onClickItem(v) }
+                            val manager = LinearLayoutManager(context).apply {
+                                orientation = LinearLayoutManager.HORIZONTAL
+                            }
 
-        listItemBinding.contentList.adapter = adapter
-        listItemBinding.contentList.layoutManager = manager
+                            contentList.adapter = adapter
+                            contentList.layoutManager = manager
+                        }
 
         return listItemBinding.root
     }
